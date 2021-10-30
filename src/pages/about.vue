82ef263c6a063d94923f82fb5b79e6ee3ff96537
jiang <jiangjf04@vanke.com>
2021-10-30 14:50:38 +0800

    feat: 新增todolist组件

--- src/pages/about.vue
+++ src/pages/about.vue
@@ -1,4 +1,23 @@
 
 <template>
-    <h1>这是关于页面</h1>
+  <div>
+    <h1 @click="add">{{ count }}</h1>
+  </div>
 </template>
+
+<script setup>
+import { ref } from "vue";
+let count = ref(1)
+let color = ref('red')
+function add() {
+  count.value++
+  color.value = Math.random()>0.5? "blue":"red"
+}
+</script>
+
+<style scoped>
+:gl
+h1 {
+  color:v-bind(color);
+}
+</style>>
--- src/pages/home.vue
+++ src/pages/home.vue
@@ -1,4 +1,9 @@
 
 <template>
     <h1>这是首页</h1>
+    <TodoList />    
 </template>
+
+<script setup>
+import TodoList from '../components/TodoList.vue'
+</script>
